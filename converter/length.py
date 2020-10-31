3d51e6118f0650c5af78769bb6f7da12a9a07cfc
addleonel <cambroni3633@gmail.com>
2020-10-31 12:14:37 -0500

    New format to lenght module

--- converter/length.py
+++ converter/length.py
@@ -1,14 +1,12 @@
 # Length units
 
-from converter.unit import Unit
-
-class Length(Unit):
+class Length:
     """
     units used
     meter, yard, mile, foot, inch, light_year
     """
     def __init__(self, value, *args):
-        super(Length, self).__init__(value)
+        self.__value = float(value)
         self.__value_convert_to_meter = args[0]
         self.__value_convert_to_yard = args[1]
         self.__value_convert_to_mile = args[2]
@@ -16,52 +14,75 @@ class Length(Unit):
         self.__value_convert_to_inch = args[4]
         self.__value_convert_to_light_year = args[5]
 
+    @classmethod
+    def from_meter(cls, value):
+        return cls(value, 1.0, 1.0936132983, 0.0006213712, 3.280839895, 39.37007874, 1.057008707E-16)
+
+    @classmethod
+    def from_yard(cls, value):
+        return cls(value, 0.9144, 1.0, 0.0005681797, 3.0, 36.0, 9.665287622E-17)
+
+    @classmethod
+    def from_mile(cls, value):
+        return cls(value, 1609.35, 1760.0065617, 1.0, 5280.019685, 63360.23622, 1.701096963E-13)
+
+    @classmethod
+    def from_foot(cls, value):
+        return cls(value, 0.3048, 0.3333333333, 0.0001893932, 1.0, 12.0, 3.22176254E-17)
+
+    @classmethod
+    def from_inch(cls, value):
+        return cls(value, 0.0254, 0.0277777778, 0.0000157828, 0.0833333333, 1.0, 2.684802117E-18)
+
+    @classmethod
+    def from_lightyear(cls, value):
+        return cls(value, 9460660000000000.0, 10346303587051618.0, 5878559666946.0,
+                   31038910761154856.0, 372466929133858300.0, 1.0)
+
+    @property
+    def value(self):
+        return self.__value
+
+    # Properties
+
+    @property
     def to_meter(self):
-        return self.value*self.__value_convert_to_meter
+        """
+        This method converts the value to Meter unit
+        """
+        return self.__value*self.__value_convert_to_meter
 
+    @property
     def to_yard(self):
-        return self.value*self.__value_convert_to_yard
+        """
+        This method converts the value to Yard unit
+        """
+        return self.__value*self.__value_convert_to_yard
 
+    @property
     def to_mile(self):
-        return self.value*self.__value_convert_to_mile
+        """
+        This method converts the value to Mile unit
+        """
+        return self.__value*self.__value_convert_to_mile
 
+    @property
     def to_foot(self):
-        return self.value*self.__value_convert_to_foot
+        """
+        This method converts the value to Foot unit
+        """
+        return self.__value*self.__value_convert_to_foot
 
+    @property
     def to_inch(self):
-        return self.value*self.__value_convert_to_inch
+        """
+        This method converts the value to Inch unit
+        """
+        return self.__value*self.__value_convert_to_inch
 
+    @property
     def to_light_year(self):
-        return self.value*self.__value_convert_to_light_year
-
-class Meter(Length):
-
-    def __init__(self, value):
-        super(Meter, self).__init__(value, 1.0, 1.0936132983, 0.0006213712, 3.280839895, 39.37007874, 1.057008707E-16)
-
-
-class Yard(Length):
-
-    def __init__(self, value):
-        super(Yard, self).__init__(value, 0.9144, 1.0, 0.0005681797, 3.0, 36.0, 9.665287622E-17)
-
-class Mile(Length):
-
-    def __init__(self, value):
-        super(Mile, self).__init__(value, 1609.35, 1760.0065617, 1.0, 5280.019685, 63360.23622, 1.701096963E-13)
-        
-class Foot(Length):
-
-    def __init__(self, value):
-        super(Foot, self).__init__(value, 0.3048, 0.3333333333, 0.0001893932, 1.0, 12.0, 3.22176254E-17)
-
-class Inch(Length):
-
-    def __init__(self, value):
-        super(Inch, self).__init__(value, 0.0254, 0.0277777778, 0.0000157828, 0.0833333333, 1.0, 2.684802117E-18)
-
-class LightYear(Length):
-
-    def __init__(self, value):
-        super(LightYear, self).__init__(value, 9460660000000000.0, 10346303587051618.0, 5878559666946.0,
-                                        31038910761154856.0, 372466929133858300.0, 1.0)
+        """
+        This method converts the value to Light Year unit
+        """
+        return self.__value*self.__value_convert_to_light_year
--- using_tools.py
+++ using_tools.py
@@ -1,40 +1,25 @@
 
-from converter.length import Meter, Yard, LightYear
-from converter.temperature import Celsius, Fahrenheit, Kelvin
-from converter.area import SquareInch, SquareFoot
-from converter.volume import Liter, CubicMeter, Milliliter
-from converter.weight import Kilogram, MetricTon
-from converter.time import Second, Minute
+from converter.length import Length
+
 
 if __name__ == '__main__':
     # length
-    print(Meter(40).to_inch())
-    print(Yard(20).to_meter())
-    print(LightYear(1).to_meter())
+    meter = Length.from_meter(23)
+    print(type(meter.value))
+    print(meter.to_mile)
+    print(meter.to_light_year)
+    print(meter.to_inch)
+    print(meter.to_foot)
+    print(meter.to_yard)
     print("-"*30)
     # temperature
-    print(Celsius(20).to_kelvin())
-    print(Celsius(20).to_fahrenheit())
-    print(Kelvin(20).to_celsius())
-    print(Kelvin(20).to_fahrenheit())
-    print(Fahrenheit(20).to_celsius())
-    print(Fahrenheit(20).to_kelvin())
-    print(Fahrenheit(20).value)
+
     # area
-    print("-"*30)
-    print(SquareInch(30).to_hectare())
-    print(SquareFoot(40).to_square_meter())
+
     # volume
-    print("-"*40)
-    print(Liter(80).to_cubic_centimeter())
-    print(Milliliter(100))
-    print(CubicMeter(0.40))
+
     # weight
-    print("-"*30)
-    print(Kilogram(300).to_gram())
-    print(MetricTon(4).to_kilogram())
+
     # time
-    print("-"*30)
-    print(Second(800).to_minute())
-    print(Minute(700).to_hour())
+
 

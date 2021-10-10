527573dcaa22fe94712133aef145b1b605fa0c16
addleonel <adleonel3633@gmail.com>
2021-10-09 23:24:26 -0500

    fix prefix

--- using_tools.py
+++ using_tools.py
@@ -2,28 +2,28 @@ from unitrevpy import convert as c
 import numpy as np
 if __name__ == '__main__':
     # length
-    num = c.Mile((23.34, 23.34, 12.3))
+    num = c.Meter((23.34, 23.34, 12.3))
     print(num.meter())
     print("-"*30)
     
     # temperature
-    tem = c.Fahrenheit({34, 12, 12, 12, 12, 12, 25, 34}).to_fahrenheit()
-    print(tem)
-    print("-" * 30)
+    # tem = c.Fahrenheit({34, 12, 12, 12, 12, 12, 25, 34}).to_fahrenheit()
+    # print(tem)
+    # print("-" * 30)
     # area
-    a = c.SquareMeter([345, 234, 123, 43]).to_square_yard()
-    print(a)
-    print("-" * 30)
+    # a = c.SquareMeter([345, 234, 123, 43]).to_square_yard()
+    # print(a)
+    # print("-" * 30)
     # volume
-    vol = c.CubicCentimeter([23, 34, 156, 43])
-    print(vol.to_liter())
-    print("-" * 30)
+    # vol = c.CubicCentimeter([23, 34, 156, 43])
+    # print(vol.to_liter())
+    # print("-" * 30)
     # weight
-    w = c.Gram(34444).to_kilogram()
-    print(w)
-    print("-" * 30)
+    # w = c.Gram(34444).to_kilogram()
+    # print(w)
+    # print("-" * 30)
     # time
-    t = c.Day([23, 12, 35, 34])
-    print(t.to_hour())
+    # t = c.Day([23, 12, 35, 34])
+    # print(t.to_hour())
 
 
